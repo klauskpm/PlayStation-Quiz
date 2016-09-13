3d7322ae500d6d01155d6f6b21ea252f970c38d8
Klaus Kazlauskas <klausmachado@concursovirtual.com.br>
2016-09-13 11:09:24 -0300

    v1.0.0
     - EditText changed type from number to text;
     - Added a hint to EditText;
     - Added verifications for CheckBoxes and EditTexts;
     - Added translation for Portuguese(BRA);
     - Adjusted the submit button style;
     - Developed the quiz logic;
     - Doing a toast message based on the score;

--- app/src/main/java/br/com/klauskpm/playstationquiz/MainActivity.java
+++ app/src/main/java/br/com/klauskpm/playstationquiz/MainActivity.java
@@ -1,7 +1,15 @@
 package br.com.klauskpm.playstationquiz;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,111 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button submitButton = (Button) findViewById(R.id.submit__button);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double points = checkAnswers();
+
+                sendMessage(points);
+            }
+        });
+    }
+
+    private double checkAnswers() {
+        double points = 0.0;
+
+        points += checkQuestion1();
+        points += checkQuestion2();
+        points += checkQuestion3();
+        points += checkQuestion4();
+
+        return points;
+    }
+
+    private void sendMessage(double points) {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        String message;
+
+        if (points == 4)
+            message = "You made a perfect score!!!";
+        else if (points < 4 && points >= 3)
+            message = "Almost perfect!!";
+        else if (points >= 2 && points < 3)
+            message = "Good, but it can be better!";
+        else
+            message = "Don't give up!";
+
+        message += "\nYou've done " + points + " out of 4";
+        Toast toast = Toast.makeText(context, message, duration);
+        toast.show();
+    }
+
+    private double checkQuestion1() {
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.question_1__radio_group);
+        int checkedId = radioGroup.getCheckedRadioButtonId();
+        if (checkedId < 0)
+            return 0;
+
+        RadioButton checkedRadioButton = (RadioButton) findViewById(checkedId);
+
+        if (checkedRadioButton.getText().toString().equals(getString(R.string.day_s_gone))) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    private double checkQuestion2() {
+        EditText editText = (EditText) findViewById(R.id.answer_question_2__edit_text);
+        String text = editText.getText().toString();
+
+        if (text != null && (text.equalsIgnoreCase("PlayStation Network") || text.equalsIgnoreCase("PSN"))) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    private double checkQuestion3() {
+        double points = 0.0;
+        double pointPerChoice = 0.5;
+
+        CheckBox PS4EliteCheckBox = (CheckBox) findViewById(R.id.answer_1_question_3__check_box);
+        CheckBox PS4SlimCheckBox = (CheckBox) findViewById(R.id.answer_2_question_3__check_box);
+        CheckBox PS4ProCheckBox = (CheckBox) findViewById(R.id.answer_3_question_3__check_box);
+        CheckBox PS4SCheckBox = (CheckBox) findViewById(R.id.answer_4_question_3__check_box);
+
+        if (PS4SlimCheckBox.isChecked())
+            points += pointPerChoice;
+
+        if (PS4ProCheckBox.isChecked())
+            points += pointPerChoice;
+
+        if (PS4SCheckBox.isChecked() && points >= pointPerChoice)
+            points -= pointPerChoice;
+
+        if (PS4EliteCheckBox.isChecked() && points >= pointPerChoice)
+            points -= pointPerChoice;
+
+        return points;
+    }
+
+    private double checkQuestion4() {
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.questio_4__radio_group);
+        int checkedId = radioGroup.getCheckedRadioButtonId();
+        if (checkedId < 0)
+            return 0;
+
+        RadioButton checkedRadioButton = (RadioButton) findViewById(checkedId);
+
+        if (checkedRadioButton.getText().toString().equals(getString(R.string.lords_and_journey))) {
+            return 1;
+        }
+
+        return 0;
     }
 }
